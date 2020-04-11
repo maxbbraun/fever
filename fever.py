71288f5f39b5b9efa38449ebee0cd2a9de31ba03
Max Braun <max.braun@gmail.com>
2020-04-10 18:28:12 -0700

    Start reading ambient sensor

--- fever.py
+++ fever.py
@@ -1,10 +1,12 @@
 from absl import app
 from absl import flags
 from absl import logging
+import bme680
 import cv2
 import cvlib as cv
 import numpy as np
 from pylepton import Lepton
+from smbus2 import SMBus
 
 FLAGS = flags.FLAGS
 flags.DEFINE_integer('min_temperature', 23715, 'The minimum temperature in'
@@ -27,6 +29,16 @@ def format_temperature(temperature):
 
 
 def main(_):
+    # Initialize ambient sensors.
+    ambient = bme680.BME680(i2c_addr=bme680.I2C_ADDR_PRIMARY,
+                            i2c_device=SMBus(1))
+    # TODO: Tune settings.
+    ambient.set_humidity_oversample(bme680.OS_2X)
+    ambient.set_pressure_oversample(bme680.OS_4X)
+    ambient.set_temperature_oversample(bme680.OS_8X)
+    ambient.set_filter(bme680.FILTER_SIZE_3)
+    ambient.set_gas_status(bme680.DISABLE_GAS_MEAS)
+
     # Initialize thermal image buffers.
     raw_buffer = np.ndarray((Lepton.ROWS, Lepton.COLS, 1), dtype=np.uint16)
     rgb_buffer = np.ndarray((Lepton.ROWS, Lepton.COLS, 3), dtype=np.uint8)
@@ -35,6 +47,17 @@ def main(_):
     # Start the data processing loop.
     with Lepton() as lepton:
         while True:
+            # Acquire ambient sensor readings.
+            if not ambient.get_sensor_data() or not ambient.data.heat_stable:
+                logging.warning('Ambient sensor data not ready')
+            ambient_data = ambient.data
+            logging.debug('Ambient temperature: %.f °C'
+                          % ambient_data.temperature)
+            logging.debug('Ambient pressure: %.f hPa'
+                          % ambient_data.pressure)
+            logging.debug('Ambient humidity: %.f %%'
+                          % ambient_data.humidity)
+
             # Get the latest frame from the thermal camera.
             lepton.capture(data_buffer=raw_buffer)
 
